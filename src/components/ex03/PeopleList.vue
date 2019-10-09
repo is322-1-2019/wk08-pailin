989870de7225c2655b286555f5b69512317d7587
pailin.h3 <jinjuta.l@st.econ.tu.ac.th>
2019-10-09 16:30:31 +0700

    week 08 first commit

--- src/components/ex03/PeopleList.vue
+++ src/components/ex03/PeopleList.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="card-content">
     <b-field label="แสดงรายชื่อทั้งหมด" v-if="people.length">
-      <b-table :data="people" :columns="columns"></b-table>
+      <b-table :data="people" 
+        :columns="columns" 
+        :selected.sync="people.deletePerson">
+      </b-table>
     </b-field>
   </div>
 </template>
@@ -11,8 +14,9 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
+        checkedRows: [],
       columns: [
-          {field: "stuId", label: "ID"},
+        {field: "stuId", label: "ID"},
         { field: "title", label: "คำนำหน้า" },
         { field: "firstName", label: "ชื่อ" },
         { field: "lastName", label: "นามสกุล" },
@@ -22,6 +26,17 @@ export default {
   },
   computed: {
     ...mapState("people", ["people"])
+  },
+  methods: {
+    deletePerson() {
+      this.$store.dispatch("people/deletePerson", {
+          stuId: this.person.stuId,
+        title: this.person.title,
+        firstName: this.person.firstName,
+        lastName: this.person.lastName,
+        isActive: this.person.isActive,
+      });
+    }
   }
 };
 </script>
--- src/store/modules/people/mutations.js
+++ src/store/modules/people/mutations.js
@@ -8,5 +8,11 @@ export const mutations = {
     let people = state.people;
     people.push(person);
     Vue.set(state, "people", people);
+  },
+  deletePerson(state, person) {
+    let people = state.people;
+    //delete person
+    person.pop();
+    Vue.set(state, "people", people);
   }
 };
